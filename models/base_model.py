d5d4d010c845c69fe6b8f8c2f84608cd7711d74b
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-01-28 17:16:35 -0800

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -13,16 +13,15 @@ class BaseModel:
         if kwargs is not None:
             my_dict = self.__dict__.copy()
             if 'created_at' in my_dict:
-                time = datetime.now().isoformat()
-                my_dict['created_at'] = datetime.fromisoformat(time)
+                my_dict['created_at'] = datetime.now()
             if 'updated_at' in my_dict:
-                time = datetime.now().isoformat()
-                my_dict['updated_at'] = datetime.fromisoformat(time)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now().isoformat()
-            self.updated_at = datetime.now().isoformat()
-            storage.new(self)
+                my_dict['updated_at'] = datetime.now()
+            if 'id' in my_dict:
+                my_dict['id'] = str(uuid.uuid4())
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        storage.new(self)
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
@@ -34,13 +33,13 @@ class BaseModel:
         if 'updated_at' in my_dict:
             time = datetime.now()
             my_dict['updated_at'] = time.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        my_dict['__class__'] = self.__class__.__name__
+        my_dict['__class__'] = type(self).__name__
         return my_dict
 
     def save(self):
         """updates the public instance attribute updated_at
         with the current datetime"""
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
         storage.save()
 
     def __str__(self):
