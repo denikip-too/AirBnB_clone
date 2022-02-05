285dc1e7574b84ef37e317819ab7da62af06b190
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-05 06:27:18 -0800

    fix

--- console.py
+++ console.py
@@ -84,8 +84,8 @@ class HBNBCommand(cmd.Cmd):
             if words[0] not in storage.classes():
                 print("** class doesn't exist **")
             else:
-                lst = [str(obj) for key, obj in storage.all().items()
-                        if type(obj).__name__ == words[0]]
+                lst = [str(obj) for key, obj in storage.all().items(
+                    ) if type(obj).__name__ == words[0]]
                 print(lst)
 
     def do_update(self, arg):
